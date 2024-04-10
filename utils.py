f0b78d87e0d01ac13cc3411a931e6ff167509f01
Darshan Jani <35656144+darshanvjani@users.noreply.github.com>
2024-04-09 20:26:38 -0700

    added VGGloss

--- utils.py
+++ utils.py
@@ -1,5 +1,24 @@
 import torch
+import torchvision.models as models
+import torch.nn.functional as F
 
+class VGGPerceptualLoss(nn.Module):
+    def __init__(self):
+        super(VGGPerceptualLoss, self).__init__()
+        vgg = models.vgg16(pretrained=True).features
+        self.slice1 = nn.Sequential(*list(vgg[:4])).eval()
+        self.slice2 = nn.Sequential(*list(vgg[4:9])).eval()
+        self.slice3 = nn.Sequential(*list(vgg[9:16])).eval()
+        for p in self.parameters():
+            p.requires_grad = False
+
+    def forward(self, x, y):
+        x = F.interpolate(x, mode='bilinear', size=(224, 224), align_corners=False)
+        y = F.interpolate(y, mode='bilinear', size=(224, 224), align_corners=False)
+        x_relu1_2, x_relu2_2, x_relu3_3 = self.slice1(x), self.slice2(x), self.slice3(x)
+        y_relu1_2, y_relu2_2, y_relu3_3 = self.slice1(y), self.slice2(y), self.slice3(y)
+        loss = F.mse_loss(x_relu1_2, y_relu1_2) + F.mse_loss(x_relu2_2, y_relu2_2) + F.mse_loss(x_relu3_3, y_relu3_3)
+        return loss
 
 class AverageMeter(object):
     """ Function to Compute and store the average and current value
